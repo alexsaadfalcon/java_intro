8b8f4e2a3adc895873ae2add6e21a1224225197c
Alex <alexsaadfalcon@yahoo.com>
2020-09-27 17:25:35 -0400

    Make and commit Javadocs script

---
+++ make_commit_docs.py
@@ -0,0 +1,18 @@
+import os, sys, shutil
+
+docdir = 'javadocs/'
+
+for n in range(1, 10):
+    hwdir = f'hw{str(n).zfill(2)}'
+    print()
+    print(hwdir)
+    print(os.listdir(hwdir))
+
+    os.chdir(hwdir)
+    os.system(f'javadoc *.java -d {hwdir}docs')
+    os.chdir('..')
+
+    shutil.move(f'{hwdir}/{hwdir}docs', f'{docdir}/')
+
+    # os.system(f'git add {docdir}/{hwdir}docs')
+    # os.system(f'git commit -m"Javadocs for homework {n}"')
